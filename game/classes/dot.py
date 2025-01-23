3232e15f3d24b1f2727139b535b67325d0a48a34
mouraa0 <jpcmouraa@gmail.com>
2025-01-23 20:52:06 -0300

    feat: advances on game loop logic

--- game/classes/dot.py
+++ game/classes/dot.py
@@ -2,36 +2,36 @@ from PPlay.sprite import *
 from PPlay.gameimage import *
 
 class Dot(Sprite):
-    def __init__(self, path=str, lane_x=int, lane_y=int, click_area=GameImage):
+    def __init__(self, path=str, lane_x=int, lane_y=int, click_area=GameImage, keyboard_key=str):
         super().__init__(path)
         self.set_position(lane_x - (self.width / 2) + 1, 0)
         self.initial_height = 0
         self.click_area = click_area
         self.points = 4
         self.center_y = self.height / 2
+        self.keyboard_key = keyboard_key
+        self.missed = False
         # self.set_curr_frame(0)
         # self.set_total_duration(1000)
         # self.set_sequence(0, 0, 0, 0)
+
 
     def move(self, delta_time):
         self.y += 140 * delta_time
 
         if self.y > self.click_area.y + self.click_area.height + 10:
-            self.y = self.initial_height
+            self.missed = True
         
         self.draw()
 
     def handle_click(self):
         hit = False
 
-        if self.y + self.height > self.click_area.y:
+        if self.y + self.height > self.click_area.y - 10:
             hit = True
-        
-        self.y = self.initial_height
 
         return hit
         
-
 
     # def set_sequence(self, start, end, repeat, speed):
     #     self.set_curr_frame(start)
--- game/game.py
+++ game/game.py
@@ -25,7 +25,7 @@ def count_points():
 
 def handle_miss():
     global multiplier_amount
-    print('here')
+    print('error')
     multiplier_amount = 1
 
 
@@ -37,6 +37,64 @@ def handle_hit():
 
     if multiplier_amount < 4:
         multiplier_amount += 1    
+
+
+def clean_active_notes(active_notes):
+    for note in active_notes:
+        if note.missed:
+            active_notes.remove(note)
+    
+    return active_notes
+
+
+def create_dot(color, lane, click_area):
+    asset_str = 'assets/game/'
+    keyboard_key = ''
+
+    if color == 'green':
+        asset_str += 'green_dot.png'
+        keyboard_key = 'd'
+    
+    elif color == 'red':
+        asset_str += 'red_dot.png'
+        keyboard_key = 'f'
+    
+    elif color == 'yellow':
+        asset_str += 'yellow_dot.png'
+        keyboard_key = 'j'
+    
+    elif color == 'blue':
+        asset_str += 'blue_dot.png'
+        keyboard_key = 'k'
+
+    elif color == 'orange':
+        asset_str += 'orange_dot.png'
+        keyboard_key = 'l'
+    
+    return Dot(asset_str, lane.x, lane.height, click_area, keyboard_key)
+
+
+def verify_key_stroke(key, active_notes):
+    # if cron != 0:
+    #     return
+
+    if teclado.key_pressed(key): 
+
+        if key == active_notes[0].keyboard_key:
+            did_hit = active_notes[0].handle_click()
+
+            if did_hit:
+                handle_hit()
+                active_notes.pop(0)
+
+                # return active_notes
+
+            # return active_notes
+
+        handle_miss()
+        # return active_notes
+
+    # return active_notes
 
 
 janela = Window(800, 600)
@@ -70,21 +128,94 @@ multiplier.set_position((janela.width / 2) - (multiplier.width / 2), points_area
 
 def game_loop():
     global points_amount
+    chart_time = 0
+    note_index = 0
+
+    click_cron = 0
+
+    chart = [
+        {"time": 1.0, "dot": create_dot('green', green_lane, green_click_area)},
+        {"time": 1.5, "dot": create_dot('red', red_lane, red_click_area)},
+        {"time": 2.0, "dot": create_dot('yellow', yellow_lane, yellow_click_area)},
+        {"time": 2.5, "dot": create_dot('blue', blue_lane, blue_click_area)},
+        {"time": 3.0, "dot": create_dot('orange', orange_lane, orange_click_area)},
+        {"time": 4.0, "dot": create_dot('green', green_lane, green_click_area)},
+        {"time": 4.0, "dot": create_dot('orange', orange_lane, orange_click_area)},
+    ]
+
+    active_notes = []
+
+    # key_states = {
+    #     'd': False,
+    #     'f': False,
+    #     'j': False,
+    #     'k': False,
+    #     'l': False
+    # }
 
     while True:
         janela.set_background_color((0, 0, 0))
+        
         if (teclado.key_pressed('esc')):
             return
         
         if (teclado.key_pressed('w')):
             count_points()
-
-        if (teclado.key_pressed('d')):
-            hit = green_dot.handle_click()
-            if hit:
-                handle_hit()
-            else:
-                handle_miss()
+        
+        if note_index < len(chart) and chart[note_index]['time'] < chart_time:
+            active_notes.append(chart[note_index]['dot'])
+            note_index += 1
+        
+        if len(active_notes) > 0:
+            verify_key_stroke('d', active_notes)
+            verify_key_stroke('f', active_notes)
+            verify_key_stroke('j', active_notes)
+            verify_key_stroke('k', active_notes)
+            verify_key_stroke('l', active_notes)
+            # if (teclado.key_pressed('d')):
+            #     if active_notes[0].keyboard_key == 'd':
+            #         hit = active_notes[0].handle_click()
+            #         active_notes.pop(0)
+            #     if hit:
+            #         handle_hit()
+            #     else:
+            #         handle_miss()
+            
+            # if (teclado.key_pressed('f')):
+            #     if active_notes[0].keyboard_key == 'f':
+            #         hit = active_notes[0].handle_click()
+            #         active_notes.pop(0)
+            #     if hit:
+            #         handle_hit()
+            #     else:
+            #         handle_miss()
+            
+            # if (teclado.key_pressed('j')):
+            #     if active_notes[0].keyboard_key == 'j':
+            #         hit = active_notes[0].handle_click()
+            #         active_notes.pop(0)
+            #     if hit:
+            #         handle_hit()
+            #     else:
+            #         handle_miss()
+            
+            # if (teclado.key_pressed('k')):
+            #     if active_notes[0].keyboard_key == 'k':
+            #         hit = active_notes[0].handle_click()
+            #         active_notes.pop(0)
+            #     if hit:
+            #         handle_hit()
+            #     else:
+            #         handle_miss()
+            
+            # if (teclado.key_pressed('l')):
+            #     if active_notes[0].keyboard_key == 'l':
+            #         hit = active_notes[0].handle_click()
+            #         active_notes.pop(0)
+            #     if hit:
+            #         handle_hit()
+            #     else:
+            #         handle_miss()
         
         green_lane.draw()
         red_lane.draw()
@@ -92,11 +223,18 @@ def game_loop():
         blue_lane.draw()
         orange_lane.draw()
 
-        green_dot.move(janela.delta_time())
-        red_dot.draw()
-        yellow_dot.draw()
-        blue_dot.draw()
-        orange_dot.draw()
+        for note in active_notes:
+            note.move(janela.delta_time())
+
+
+        active_notes = clean_active_notes(active_notes)
+
+
+        # green_dot.move(janela.delta_time())
+        # red_dot.draw()
+        # yellow_dot.draw()
+        # blue_dot.draw()
+        # orange_dot.draw()
 
         green_click_area.draw()
         red_click_area.draw()
@@ -109,4 +247,5 @@ def game_loop():
 
         multiplier.draw()
 
+        chart_time += janela.delta_time()
         janela.update()
