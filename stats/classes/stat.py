fed18787d0ac3e840054cb362c837a37129063d0
mouraa0 <jpcmouraa@gmail.com>
2025-01-24 04:06:14 -0300

    feat: stats screen

---
+++ stats/classes/stat.py
@@ -0,0 +1,7 @@
+class Stat:
+    def __init__(self, date, hour, score, highest_streak, misses):
+        self.date = date
+        self.hour = hour
+        self.score = score
+        self.highest_streak = highest_streak
+        self.misses = misses
---
+++ stats/stats.py
@@ -0,0 +1,26 @@
+from PPlay.keyboard import *
+from PPlay.window import *
+from stats.classes.stat import Stat
+
+janela = Window(800, 600)
+teclado = Keyboard()
+
+def stats_loop(stat=Stat, is_end_game=False):
+   while True:
+        janela.set_background_color((0, 0, 0))
+
+        janela.draw_text('Stats', 20, 20, size=30, color=(255, 255, 255), font_name='Arial', bold=True, italic=False)
+
+        janela.draw_text(f'Date: {stat.date}', 20, 100, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Hour: {stat.hour}', 20, 150, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Score: {stat.score}', 20, 200, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Highest streak: {stat.highest_streak}', 20, 250, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Misses: {stat.misses}', 20, 300, size=30, color=(255, 255, 255))
+
+        janela.draw_text(f'Press ESC to {"continue" if is_end_game else "go back"}', janela.width - 320, janela.height - 80, size=30, color=(255, 255, 255))
+
+        if (teclado.key_pressed('esc')):
+            return
+
+        janela.update()
+
