76b059c3129d2de063b9bc2f98d3c7872c6f58e9
mouraa0 <jpcmouraa@gmail.com>
2025-01-24 05:57:24 -0300

    feat: stats screen to portuguese

--- stats/stats.py
+++ stats/stats.py
@@ -34,13 +34,15 @@ def stats_loop(stat=Stat, is_end_game=False):
 
         janela.draw_text('Stats', 20, 20, size=30, color=(255, 255, 255), font_name='Arial', bold=True, italic=False)
 
-        janela.draw_text(f'Date: {stat.date}', 20, 100, size=30, color=(255, 255, 255))
-        janela.draw_text(f'Hour: {stat.hour}', 20, 150, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Data: {stat.date}', 20, 100, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Hora: {stat.hour}', 20, 150, size=30, color=(255, 255, 255))
         janela.draw_text(f'Score: {stat.score}', 20, 200, size=30, color=(255, 255, 255))
-        janela.draw_text(f'Highest streak: {stat.highest_streak}', 20, 250, size=30, color=(255, 255, 255))
-        janela.draw_text(f'Misses: {stat.misses}', 20, 300, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Maior streak: {stat.highest_streak}', 20, 250, size=30, color=(255, 255, 255))
+        janela.draw_text(f'Erros: {stat.misses}', 20, 300, size=30, color=(255, 255, 255))
 
-        janela.draw_text(f'Press ESC to {"continue" if is_end_game else "go back"}', janela.width - 320, janela.height - 80, size=30, color=(255, 255, 255))
+        distance = 400 if is_end_game else 350
+
+        janela.draw_text(f'Aperte ESC para {"continuar" if is_end_game else "voltar"}', janela.width - distance, janela.height - 80, size=30, color=(255, 255, 255))
 
         if (teclado.key_pressed('esc')):
             return
